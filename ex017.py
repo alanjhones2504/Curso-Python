ca6be2d9e6ef218dda77dfca7494d1de8e7166c1
alan jhones <alanjpb250482@gmail.com>
2025-02-12 10:39:13 -0300

    range Python-projetos

---
+++ ex017.py
@@ -0,0 +1,14 @@
+v = []
+s = 0 
+
+for i in range(5):
+    dado = int(input("Digite um numero inteiro: "))
+    v.append(dado)
+    s += dado
+
+media = s / 5
+
+for elem in v:
+    print(elem, " ", end=" ")
+
+print(f"Média dos elementos: {media}")
