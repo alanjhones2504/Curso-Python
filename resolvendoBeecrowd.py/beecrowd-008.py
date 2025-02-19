972f83cca9b1119580dc5ebe72cd7e28091dc98c
alan jhones <alanjpb250482@gmail.com>
2025-02-19 16:54:51 -0300

    resolvendo beecrowd

--- resolvendoBeecrowd.py/beecrowd-008.py
+++ resolvendoBeecrowd.py/beecrowd-008.py
@@ -1,4 +1,11 @@
-numFuc = int(input())
-numHoras = int(input())
-valorHoras = float(input())
 
+numero_funcionario = int(input())
+horas_trabalhadas = int(input())
+valor_por_hora = float(input())
+
+
+salario = horas_trabalhadas * valor_por_hora
+
+
+print(f'NUMBER = {numero_funcionario}')
+print(f'SALARY = U$ {salario:.2f}')
