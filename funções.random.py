8e871d7c7dae01b0460a84ab9c3d5cf572050d46
alan jhones <117799373+alanjhones2504@users.noreply.github.com>
2025-04-06 12:05:02 -0300

    Atualizar o funções.random.py

--- funções.random.py
+++ funções.random.py
@@ -6,3 +6,12 @@
 # random.random() - gera um numero aleatorio entre 0 e 1
 # random.choice([lista_itens])
 
+import random
+
+opcoes = ["cara", "coroa"]
+resultado = random.choice(opcoes)
+print(f"O resultado do lançamento foi: {resultado}")
+
+
+
+
